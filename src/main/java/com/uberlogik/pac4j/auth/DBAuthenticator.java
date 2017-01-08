7fb027337351ca48ee57473029f859f89ea467c2
Victor Noël <victor.noel@crazydwarves.org>
2017-01-08 21:23:06 +0100

    Relies on pac4j PasswordEncoder
    Instead of homemade SaltedPassword, use proven PBKDF2

--- src/main/java/com/uberlogik/pac4j/auth/DBAuthenticator.java
+++ src/main/java/com/uberlogik/pac4j/auth/DBAuthenticator.java
@@ -1,25 +1,27 @@
 package com.uberlogik.pac4j.auth;
 
-import org.pac4j.core.context.WebContext;
-import org.pac4j.core.credentials.UsernamePasswordCredentials;
-import org.pac4j.core.credentials.authenticator.Authenticator;
-import org.pac4j.core.exception.*;
-import org.pac4j.core.profile.CommonProfile;
-import org.pac4j.core.util.InitializableWebObject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
+
+import org.pac4j.core.context.WebContext;
+import org.pac4j.core.credentials.UsernamePasswordCredentials;
+import org.pac4j.core.credentials.authenticator.AbstractUsernamePasswordAuthenticator;
+import org.pac4j.core.exception.AccountNotFoundException;
+import org.pac4j.core.exception.BadCredentialsException;
+import org.pac4j.core.exception.CredentialsException;
+import org.pac4j.core.exception.HttpAction;
+import org.pac4j.core.exception.MultipleAccountsFoundException;
+import org.pac4j.core.profile.CommonProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Enables use of local database via your preferred method (JOOQ, JDBI, JDBC or an ORM)
  *
  * @Author Rob Ward
  */
-public abstract class DBAuthenticator<T> extends InitializableWebObject implements Authenticator<UsernamePasswordCredentials>
+public abstract class DBAuthenticator<T> extends AbstractUsernamePasswordAuthenticator
 {
     private static final Logger LOG = LoggerFactory.getLogger(DBAuthenticator.class);
 
@@ -54,8 +56,7 @@ public abstract class DBAuthenticator<T> extends InitializableWebObject implemen
 
         T user = users.iterator().next();
 
-        SaltedPassword password = saltedPassword(user);
-        if (password.matches(credentials.getPassword()))
+        if (getPasswordEncoder().matches(credentials.getPassword(), password(user)))
         {
             loginSuccessful(user);
             Map<String, Set<String>> rolePermissions = getRolePermissions(user);
@@ -71,7 +72,7 @@ public abstract class DBAuthenticator<T> extends InitializableWebObject implemen
 
     protected abstract Collection<T> getUsers(String username);
 
-    protected abstract SaltedPassword saltedPassword(T user);
+    protected abstract String password(T user);
 
     /**
      * Returns a Map, with contents:
